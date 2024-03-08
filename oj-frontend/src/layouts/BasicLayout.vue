1a156692233555af5e69daedd46e4e692f8fe9e8
roger <nighterdream123@gmail.com>
2024-03-09 01:28:03 +0800

    完成了页面的基本布局

--- oj-frontend/src/layouts/BasicLayout.vue
+++ oj-frontend/src/layouts/BasicLayout.vue
@@ -1,5 +1,45 @@
 <template>
-  <router-view />
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <GlobalHeader></GlobalHeader>
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="http://www.nighterdream.cn/" class="href">nighterdream</a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
 </template>
 
-<style></style>
+<style scoped>
+#basicLayout .header {
+  margin-bottom: 10px;
+  box-shadow: #eee 1px 1px 5px;
+}
+
+#basicLayout .content {
+  background: linear-gradient(#e66465, #9198e5);
+  margin-bottom: 16px;
+}
+
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+
+<script>
+import GlobalHeader from "@/components/GlobalHeader.vue";
+
+export default {
+  components: { GlobalHeader },
+};
+</script>
