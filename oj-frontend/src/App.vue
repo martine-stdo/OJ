16e14fdde2b0b13d5e4572fe6ae7ed09c1ba9252
roger <nighterdream123@gmail.com>
2024-03-10 14:20:23 +0800

    添加了全局启动函数模块

--- oj-frontend/src/App.vue
+++ oj-frontend/src/App.vue
@@ -1,11 +1,41 @@
 <template>
   <div class="app">
-    <BasicLayout></BasicLayout>
+    <BasicLayout />
   </div>
 </template>
 
-<style>
-#app {
-  background-attachment: fixed;
-}
-</style>
+<style></style>
+
+<script setup lang="ts">
+import BasicLayout from "@/layouts/BasicLayout";
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+
+const router = useRouter();
+const store = useStore();
+/**
+ * 全局初始化函数，全局单次调用的代码都可以写在这里
+ */
+const doInit = () => {
+  console.log();
+};
+
+onMounted(() => {
+  doInit();
+});
+
+//权限管理
+
+//在公共组件中获取下一个路由信息中是否需要meta.access并且通过
+//状态管理获取用户权限来判断是否跳转
+router.beforeEach((to, from, next) => {
+  if (to.meta?.access === "canAdmin") {
+    if (store.state.user.loginUser?.role !== "admin") {
+      next("/404");
+      return;
+    }
+  }
+  next();
+});
+</script>
