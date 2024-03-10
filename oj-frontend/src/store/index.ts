950bb4968940fb02b3e3d8e0727f0c621919f725
roger <nighterdream123@gmail.com>
2024-03-10 14:24:34 +0800

    完成了用户状态管理模块

--- oj-frontend/src/store/index.ts
+++ oj-frontend/src/store/index.ts
@@ -1,9 +1,19 @@
+// 引入 Vuex 中的 createStore 方法
 import { createStore } from "vuex";
 
+// 引入名为 "user" 的模块
+import user from "./user";
+
+// 创建 Vuex store 实例
 export default createStore({
-  state: {},
-  getters: {},
+  // 根模块中的 mutations（用于同步修改状态）
   mutations: {},
+
+  // 根模块中的 actions（用于包含异步逻辑的操作）
   actions: {},
-  modules: {},
+
+  // 根模块中的 modules（用于引入其他模块，这里引入了名为 "user" 的模块）
+  modules: {
+    user, // 将名为 "user" 的模块添加到根模块中
+  },
 });
---
+++ oj-frontend/src/store/user.ts
@@ -0,0 +1,37 @@
+// 引入 Vuex 中的 StoreOptions 类型
+import { ACCESS_ENUM } from "@/access/accessEnum";
+import { StoreOptions } from "vuex";
+// 导出 Vuex store 的配置对象
+export default {
+  // 命名空间，使得模块化 Vuex store 更容易管理
+  namespaced: true,
+
+  // 定义 Vuex store 的初始状态
+  state: () => ({
+    loginUser: {
+      userName: "未登录",
+      userRole: ACCESS_ENUM.NOT_LOGIN,
+    },
+  }),
+
+  // 定义异步操作，可通过提交 mutations 来修改状态
+  actions: {
+    // 异步操作，获取登录用户信息
+    // 参数包括 commit 方法用于提交 mutations，state 用于访问当前状态，payload 用于传递额外的数据
+    getLoginUser({ commit, state }, payload) {
+      // todo 从远程获取登录信息
+      // 提交更新用户信息的 mutations
+      commit("updateUser", payload);
+    },
+  },
+
+  // 定义同步操作，通过直接修改状态来实现
+  mutations: {
+    // 更新用户信息的 mutations
+    // 参数包括当前状态对象 state 和负载数据 payload
+    updateUser(state, payload) {
+      // 更新用户信息
+      state.loginUser = payload;
+    },
+  },
+} as StoreOptions<any>;
